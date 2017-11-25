9bb97a12f9ba20fd807f1cecef01a7b433d7a6cb
KevenMax <kmax136@gmail.com>
2017-11-24 22:39:11 -0300

    Envio de função sorteio e músicas

--- start.c
+++ start.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <time.h>
-#include <string.h>
+
 
 struct player{
     char nome[30];
@@ -10,7 +10,7 @@ struct player{
 };
 struct player play1,play2;
 char vet_pergunta[16],vet_resposta[16];
-
+int pontuacao_rodada;
 
 
 
@@ -28,16 +28,24 @@ void sleep(time_t delay){
 void painel();
 void op_menu( int op);
 void cadastro_player();
+void sorteio_pontuacao();
+void start();
+
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
     painel();
+    start();
+
+
 
     return 0;
 }
 
 void painel(){
+
+    system("start arquivos/roda_roda.mp3");
     int op;
     puts("");
     puts("               *********    ***********    ********           ******                             ");
@@ -74,6 +82,7 @@ void painel(){
 
 
 void op_menu(int op){
+    system("start arquivos/null.mp3");
     int valor_setado;
     switch(op){
         case 0:
@@ -95,7 +104,7 @@ void cadastro_player(){
     scanf(" %29[^\n]s", play1.nome);
     printf("               Digite o Player 2:");
     scanf(" %29[^\n]s", play2.nome);
-    carregar_arquivo();
+
 }
 
 void carregar_arquivo(){
@@ -106,7 +115,7 @@ void carregar_arquivo(){
 
     system("cls");
     printf("\n \n \n \n");
-    puts("               Iniciando o Jogo...");
+    puts("                                      Iniciando o Jogo...");
     sleep(2);
     system("cls");
 
@@ -123,7 +132,7 @@ void carregar_arquivo(){
     }
     else{
      while(fscanf(fp,"%s",vet_pergunta)!= EOF){
-             printf("DICA: %s \n \n",vet_pergunta);
+        printf(" ");
      }
 
     }
@@ -136,9 +145,185 @@ void carregar_arquivo(){
     }
     else{
      while(fscanf(fp,"%s",vet_resposta)!= EOF)
-        printf("REPOSTA: %s",vet_resposta);
+        printf(" ");
+
     }
     fclose (fp);
 }
+void sorteio_pontuacao(){
+srand(time(NULL));
+
+int i,j,cres;
+int valores_pontos[25]={10,10,15,20,20,25,30,30,40,40,40,50,50,60,60,70,75,80,80,90,100,1,1,1,0};
+int v[15];
+
+for(j=0;j<7;j++){
+    for(i=0;i<15;i++){
+        v[i]=rand()%25;
+
+    }
+    if(j<6){
+        /*VISUAL DO SORTEIO 1*/
+        if(j%2==0){
+                printf("\n\n                 --------------------------------------------------------------------\n");
+                    printf("                 ||       BRUXA   = 0    ||      -> PERDE TUDO E PASSA A VEZ!      ||\n");
+                    printf("                 --------------------------------------------------------------------\n");
+                    printf("                 ||     AZARENTO  = 1    ||      ->     PASSA A VEZ!               ||\n");
+                    printf("                 --------------------------------------------------------------------\n");
+                    printf("\n");
+                    printf("                                                  ___                          \n");
+                    printf("                                                 |   |                           \n");
+                    printf("                                                 |   |                           \n");
+                    printf("                                              _ _|   |_ _                        \n");
+                    printf("                                              \\         /                        \n");
+                    printf("                                               \\       /                         \n");
+                    printf("                                                \\     /                          \n");
+                    printf("                                                 \\   /                           \n");
+                    printf("                                                  \\ /                            \n");
+                    printf("                                                   v                                       \n");
+                    printf("                                                                                      \n");
+                    printf("      *******-----*******-----*******-----*******-----*******-----*******-----*******-----*******\n");
+                    printf("      *     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *\n");
+                    printf("      * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d *\n",valores_pontos[v[0]],valores_pontos[v[1]],valores_pontos[v[2]],valores_pontos[v[3]],valores_pontos[v[4]],valores_pontos[v[5]],valores_pontos[v[6]],valores_pontos[v[7]],valores_pontos[v[8]],valores_pontos[v[9]],valores_pontos[v[10]],valores_pontos[v[11]],valores_pontos[v[12]],valores_pontos[v[13]],valores_pontos[v[14]]);
+                    printf("      *     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *\n");
+                    printf("      *******-----*******-----*******-----*******-----*******-----*******-----*******-----*******\n");
+                    printf("                                                                                      \n");
+        }
+        /*VISUAL DO SORTEIO 2*/
+        else{
+                printf("\n\n                 --------------------------------------------------------------------\n");
+                    printf("                 ||       BRUXA   = 0    ||      -> PERDE TUDO E PASSA A VEZ!      ||\n");
+                    printf("                 --------------------------------------------------------------------\n");
+                    printf("                 ||     AZARENTO  = 1    ||      ->     PASSA A VEZ!               ||\n");
+                    printf("                 --------------------------------------------------------------------\n");
+                    printf("\n");
+                    printf("                                                  ___                          \n");
+                    printf("                                                 |   |                           \n");
+                    printf("                                                 |   |                           \n");
+                    printf("                                              _ _|   |_ _                        \n");
+                    printf("                                              \\         /                        \n");
+                    printf("                                               \\       /                         \n");
+                    printf("                                                \\     /                          \n");
+                    printf("                                                 \\   /                           \n");
+                    printf("                                                  \\ /                            \n");
+                    printf("                                                   v                                       \n");
+                    printf("                                                                                      \n");
+                    printf("       -----*******-----*******-----*******-----*******-----*******-----*******-----*******-----\n");
+                    printf("      |     *     *     *     *     *     *     *     *     *     *     *     *     *     *     |\n");
+                    printf("      | %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d * %3d |\n",valores_pontos[v[0]],valores_pontos[v[1]],valores_pontos[v[2]],valores_pontos[v[3]],valores_pontos[v[4]],valores_pontos[v[5]],valores_pontos[v[6]],valores_pontos[v[7]],valores_pontos[v[8]],valores_pontos[v[9]],valores_pontos[v[10]],valores_pontos[v[11]],valores_pontos[v[12]],valores_pontos[v[13]],valores_pontos[v[14]]);
+                    printf("      |     *     *     *     *     *     *     *     *     *     *     *     *     *     *     |\n");
+                    printf("       -----*******-----*******-----*******-----*******-----*******-----*******-----*******-----\n");
+                    printf("                                                                                      \n");
+        }
+    }
+    else{
+        /*RUXA V[7] == 0*/
+        if(valores_pontos[v[7]]==0){
+            printf("\n\n                                  ------------------------------------\n");
+                printf("                                 |   ****  ****  *   * *   *  ***     |\n");
+                printf("                                 |   *   * *   * *   *  * *  *   *    |\n");
+                printf("                                 |   ****  ****  *   *   *   *****    |\n");
+                printf("                                 |   *   * *  *  *   *  * *  *   *    |\n");
+                printf("                                 |   ****  *   *  ***  *   * *   *    |\n");
+                printf("                                 |  PERDEU TUDO!!! E PASSOU A VEZ!!!  |\n");
+                printf("                                  ------------------------------------ \n");
+                printf("                                                  ___                           \n");
+                printf("                                                 |***|                           \n");
+                printf("                                                 |***|                           \n");
+                printf("                                              _ _|***|_ _                        \n");
+                printf("                                              \\*********/                        \n");
+                printf("                                               \\*******/                         \n");
+                printf("                                                \\*****/                          \n");
+                printf("                                                 \\***/                           \n");
+                printf("                                                  \\*/                            \n");
+                printf("                                                   v                                       \n");
+                printf("                                               *********                                      \n");
+                printf("      ----- ----- ----- ----- ----- ----- -----*********----- ----- ----- ----- ----- ----- -----\n");
+                printf("     |     |     |     |     |     |     |     **     **     |     |     |     |     |     |     |\n");
+                printf("     | %3d | %3d | %3d | %3d | %3d | %3d | %3d ** %3d ** %3d | %3d | %3d | %3d | %3d | %3d | %3d |\n",valores_pontos[v[0]],valores_pontos[v[1]],valores_pontos[v[2]],valores_pontos[v[3]],valores_pontos[v[4]],valores_pontos[v[5]],valores_pontos[v[6]],valores_pontos[v[7]],valores_pontos[v[8]],valores_pontos[v[9]],valores_pontos[v[10]],valores_pontos[v[11]],valores_pontos[v[12]],valores_pontos[v[13]],valores_pontos[v[14]]);
+                printf("     |     |     |     |     |     |     |     **     **     |     |     |     |     |     |     |\n");
+                printf("      ----- ----- ----- ----- ----- ----- -----*********----- ----- ----- ----- ----- ----- -----\n");
+                printf("                                               *********                                          \n");
+        }
+        else{
+            /*AZARENTO V[7] == 1*/
+            if(valores_pontos[v[7]]==1){
+            printf("\n\n                          -----------------------------------------------------\n");
+                printf("                         |    ***  *****  ***  ****  ***** *   * *****  ***    |\n");
+                printf("                         |   *   *    *  *   * *   * *     **  *   *   *   *   |\n");
+                printf("                         |   *****   *   ***** ****  ****  * * *   *   *   *   |\n");
+                printf("                         |   *   *  *    *   * *  *  *     *  **   *   *   *   |\n");
+                printf("                         |   *   * ***** *   * *   * ***** *   *   *    ***    |\n");
+                printf("                         |                  PASSOU A VEZ!!!                    |\n");
+                printf("                          -----------------------------------------------------\n");
+                printf("                                                  ___                           \n");
+                printf("                                                 |***|                           \n");
+                printf("                                                 |***|                           \n");
+                printf("                                              _ _|***|_ _                        \n");
+                printf("                                              \\*********/                        \n");
+                printf("                                               \\*******/                         \n");
+                printf("                                                \\*****/                          \n");
+                printf("                                                 \\***/                           \n");
+                printf("                                                  \\*/                            \n");
+                printf("                                                   v                                       \n");
+                printf("                                               *********                                      \n");
+                printf("      ----- ----- ----- ----- ----- ----- -----*********----- ----- ----- ----- ----- ----- -----\n");
+                printf("     |     |     |     |     |     |     |     **     **     |     |     |     |     |     |     |\n");
+                printf("     | %3d | %3d | %3d | %3d | %3d | %3d | %3d ** %3d ** %3d | %3d | %3d | %3d | %3d | %3d | %3d |\n",valores_pontos[v[0]],valores_pontos[v[1]],valores_pontos[v[2]],valores_pontos[v[3]],valores_pontos[v[4]],valores_pontos[v[5]],valores_pontos[v[6]],valores_pontos[v[7]],valores_pontos[v[8]],valores_pontos[v[9]],valores_pontos[v[10]],valores_pontos[v[11]],valores_pontos[v[12]],valores_pontos[v[13]],valores_pontos[v[14]]);
+                printf("     |     |     |     |     |     |     |     **     **     |     |     |     |     |     |     |\n");
+                printf("      ----- ----- ----- ----- ----- ----- -----*********----- ----- ----- ----- ----- ----- -----\n");
+                printf("                                               *********                                          \n");
+            }
+            /*PONTUAÇAO VALIDA!!*/
+            else{
+            printf("\n\n                       -----------------------------------------------------\n");
+                printf("                      |                     SUA RODADA VALE                 |\n");
+                printf("                      |                                                     |\n");
+                printf("                      |                       **********                    |\n");
+                printf("                      |                       *  %3d   *                    |\n",valores_pontos[v[7]]);
+                printf("                      |                       **********                    |\n");
+                printf("                      |                     PONTOS POR LETRA                |\n");
+                printf("                       -----------------------------------------------------\n");
+                printf("                                                  ___                           \n");
+                printf("                                                 |***|                           \n");
+                printf("                                                 |***|                           \n");
+                printf("                                              _ _|***|_ _                        \n");
+                printf("                                              \\*********/                        \n");
+                printf("                                               \\*******/                         \n");
+                printf("                                                \\*****/                          \n");
+                printf("                                                 \\***/                           \n");
+                printf("                                                  \\*/                            \n");
+                printf("                                                   v                                       \n");
+                printf("                                               *********                                      \n");
+                printf("      ----- ----- ----- ----- ----- ----- -----*********----- ----- ----- ----- ----- ----- -----\n");
+                printf("     |     |     |     |     |     |     |     **     **     |     |     |     |     |     |     |\n");
+                printf("     | %3d | %3d | %3d | %3d | %3d | %3d | %3d ** %3d ** %3d | %3d | %3d | %3d | %3d | %3d | %3d |\n",valores_pontos[v[0]],valores_pontos[v[1]],valores_pontos[v[2]],valores_pontos[v[3]],valores_pontos[v[4]],valores_pontos[v[5]],valores_pontos[v[6]],valores_pontos[v[7]],valores_pontos[v[8]],valores_pontos[v[9]],valores_pontos[v[10]],valores_pontos[v[11]],valores_pontos[v[12]],valores_pontos[v[13]],valores_pontos[v[14]]);
+                printf("     |     |     |     |     |     |     |     **     **     |     |     |     |     |     |     |\n");
+                printf("      ----- ----- ----- ----- ----- ----- -----*********----- ----- ----- ----- ----- ----- -----\n");
+                printf("                                               *********                                          \n");
+            }
+        }
+    }
+
+    if(j<6){
+        sleep(1);
+        system("cls");
+    }
+}
+
+if(j==7)
+    system("pause");
 
 
+
+pontuacao_rodada = v[7];
+
+}
+
+
+
+void start(){
+    carregar_arquivo();
+    sorteio_pontuacao();
+
+}
