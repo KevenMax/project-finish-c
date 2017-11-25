e6e2a7d524b759f75ba26be89594a348ac05a5db
luis <luis.fernandolmuniz@gmail.com>
2017-11-25 19:06:43 -0200

    quase terminando

--- start.c
+++ start.c
@@ -3,50 +3,71 @@
 #include <locale.h>
 #include <time.h>
 
-
 struct player{
     char nome[30];
     int pontos;
 };
+
+/*variaveis globais*/
 struct player play1,play2;
 char vet_pergunta[16],vet_resposta[16];
-int pontuacao_rodada;
 
-
-
-void sleep(time_t delay){
-    time_t timer0, timer1;
-    time(&timer0);
-    do{
-        time(&timer1);
-    }while((timer1-timer0) < delay);
-}
-
-
-
-
-void painel();
-void op_menu( int op);
+//funcoes usadas
+void painel_jogada(int *P);
+void sleep(time_t delay);
+void painel(int *P_pont_rodada);
 void cadastro_player();
 void sorteio_pontuacao();
 void start();
 
-
 int main(){
-    setlocale(LC_ALL, "Portuguese");
+setlocale(LC_ALL, "Portuguese");
+int op,ganhador;
 
-    painel();
-    rodada();
+//variaveis usadas na funcao do sorteio
+int pontos_rodada=0;
 
+    //inciando o menu do jogo
+    do{
+        painel(&op);
+        if(op<0||op>2){
+            printf("\nOpção Inválida! Digite outra opção!\n");
+            system("pause");
+            system("cls");
+        }
+    }while(op<0||op>1);
+    //iniando algumas variaveis antes de comecar o jogo:
+    play1.pontos = 0; play2.pontos = 0;
 
+        //sair do jogo -> 0
+        if(op==0){
+            system("cls");
+            printf("Saindo...");
+            exit(0);
+        }
 
-    return 0;
+        //iniciar o jogo ->1
+        if(op==1){
+            //cadastro dos players
+            cadastro_player();
+            //carregando a palavra a ser decifrada
+            carregar_arquivo();
+
+            //loop do sorteio ate sair um vencedor, caso houver um vencedor, volta pro menu do jogo
+            ganhador=1;
+            do{
+                painel_jogada(&pontos_rodada);
+                sorteio_pontuacao(&pontos_rodada);
+            }while(ganhador==1);
+        }
+
+return 0;
 }
 
-void painel(){
+void painel(int *P){
 
-    system("start arquivos/roda_roda.mp3");
-    int op;
+    //system("start arquivos/roda_roda.mp3");
+    //int op;
     puts("");
     puts("               *********    ***********    ********           ******                             ");
     puts("               ***   ***    ***********    **********        ********                            ");
@@ -74,28 +95,10 @@ void painel(){
     puts("               |                       1 - Iniciar                         |    ");
     puts("               |                       2 - Ranking                         |    ");
     puts("               |-----------------------------------------------------------|    ");
-    puts("               Digite sua opção: ");
-    printf("               ");
-    scanf("%d",&op);
-    op_menu(op);
-}
+    printf("               Digite sua opção: ");
+    scanf("%d",&*P);
+    //op_menu(op);
 
-
-void op_menu(int op){
-    system("start arquivos/null.mp3");
-    int valor_setado;
-    switch(op){
-        case 0:
-            system("cls");
-            exit(0);
-            break;
-        case 1:
-            cadastro_player();
-            break;
-        default:
-            puts("               Opção inválida, tente novamente:");
-            break;
-    }
 }
 
 void cadastro_player(){
@@ -125,32 +128,128 @@ void carregar_arquivo(){
 
     url_resposta[10] = url_tema[10] = sort + '0';
 
+    //carregando um tema
     fp = fopen(url_tema, "r");
     if (fp == NULL) {
        printf ("Houve um erro ao abrir o arquivo.\n");
        exit(0);
     }
     else{
-     while(fscanf(fp,"%s",vet_pergunta)!= EOF){
-        printf(" ");
-     }
-
+         while(fscanf(fp,"%s",vet_pergunta)!= EOF){
+            printf(" ");
+         }
     }
     fclose (fp);
-
+    //carregando a resposta do tema acima
     fp = fopen(url_resposta, "r");
     if (fp == NULL) {
        printf ("Houve um erro ao abrir o arquivo.\n");
        exit(0);
     }
     else{
-     while(fscanf(fp,"%s",vet_resposta)!= EOF)
-        printf(" ");
-
+         while(fscanf(fp,"%s",vet_resposta)!= EOF)
+            printf(" ");
     }
     fclose (fp);
 }
-void sorteio_pontuacao(){
+
+void sleep(time_t delay){
+    time_t timer0, timer1;
+    time(&timer0);
+    do{
+        time(&timer1);
+    }while((timer1-timer0) < delay);
+}
+
+void painel_jogada(int *P_pont_rodada){
+//contadores
+int z,y;
+
+char resposta[20];
+char letras_digitadas[26]={"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"};
+char mostrar_letras[16]="_______________";
+char letra;
+printf("%s\n\n",letras_digitadas);
+int sair;
+
+for(z=0;z<16;z++){
+    if(z<strlen(vet_resposta))
+        mostrar_letras[z]='_';
+    else
+        mostrar_letras[z]=' ';
+}
+
+
+do{
+    sair=0;
+
+    printf(" ---------------------------------------------------------------------------------------------\n");
+    printf("|                                                                    |      PLACAR ATUAL      |\n");
+    printf("|                                                                    | -----------------------|\n");
+    printf("|           ESSA RODADA VALE %3d PONTOS POR LETRA                    |  %10s  : %4d    |\n",*P_pont_rodada,play1.nome,play1.pontos);
+    printf("|                                                                    |                        |\n");
+    printf("|                                                                    |  %10s  : %4d    |\n",play2.nome,play2.pontos);
+    printf("|---------------------------------------------------------------------------------------------\n");
+    printf("|                                                                                             |\n");
+    printf("|                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                 |\n");
+    printf("|                x                                                          x                 |\n");
+    printf("|                x               TEMA ->%20s                x                 |\n",vet_pergunta);
+    printf("|                x                                                          x                 |\n");
+    printf("|                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                 |\n");
+    printf("|                x                                                          x                 |\n");
+    printf("|                x        %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c                     x                 |\n",mostrar_letras[0],mostrar_letras[1],mostrar_letras[2],mostrar_letras[3],mostrar_letras[4],mostrar_letras[5],mostrar_letras[6],mostrar_letras[7],mostrar_letras[8],mostrar_letras[9],mostrar_letras[10],mostrar_letras[11],mostrar_letras[12],mostrar_letras[13],mostrar_letras[14]);
+    printf("|                x                                                          x                 |\n");
+    printf("|                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                 |\n");
+    printf("|                                                                                             |\n");
+    printf("|                                                                                             |\n");
+    printf("|---------------------------------------------------------------------------------------------|\n");
+    printf("|LETRAS JA DIGITADAS:                                                                         |\n");
+    printf(" ---------------------------------------------------------------------------------------------\n");
+    printf("Digite uma letra: ");
+    scanf(" %c",&letra);
+
+    for(z=0;z<26;z++){
+        if(letras_digitadas[z]==letra){
+            printf("Letra ja foi digitada...\n\n");
+            sair=1;
+            system("pause");
+            break;
+        }
+    }
+system("cls");
+}while(sair==1);
+
+system("cls");
+
+//TESTE PRA VE SE TEM ALGUMA LETRA JÁ DIGITADA
+//for(z=0;z<strlen())
+for(z=0;z<strlen(resposta);z++){
+    if(resposta[z]==letra)
+        mostrar_letras[z]=letra;
+}
+printf(" ---------------------------------------------------------------------------------------------\n");
+printf("|                                                                    |      PLACAR ATUAL      |\n");
+printf("|                                                                    | -----------------------|\n");
+printf("|           ESSA RODADA VALE %3d PONTOS POR LETRA                    |    Player 1: %4d      |\n",*P_pont_rodada,play1.pontos);
+printf("|                                                                    |                        |\n");
+printf("|                                                                    |    Player 2: %4d      |\n",play2.pontos);
+printf("|---------------------------------------------------------------------------------------------\n");
+printf("|                                                                                             |\n");
+printf("|                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                 |\n");
+printf("|                x                                                          x                 |\n");
+printf("|                x               TEMA ->%20s                x                 |\n",vet_pergunta);
+printf("|                x                                                          x                 |\n");
+printf("|                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                 |\n");
+printf("|                x                                                          x                 |\n");
+printf("|                x        %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c                     x                 |\n",mostrar_letras[0],mostrar_letras[1],mostrar_letras[2],mostrar_letras[3],mostrar_letras[4],mostrar_letras[5],mostrar_letras[6],mostrar_letras[7],mostrar_letras[8],mostrar_letras[9],mostrar_letras[10],mostrar_letras[11],mostrar_letras[12],mostrar_letras[13],mostrar_letras[14]);
+printf("|                x                                                          x                 |\n");
+printf("|                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                 |\n");
+printf("|                                                                                             |\n");
+printf("|                                                                                             |\n");
+printf(" ---------------------------------------------------------------------------------------------\n");
+}
+
+void sorteio_pontuacao(int *P_pont_rodada){
 srand(time(NULL));
 
 int i,j,cres;
@@ -316,14 +415,5 @@ if(j==7)
 
 
 
-pontuacao_rodada = v[7];
-
-}
-
-
-
-void rodada(){
-    carregar_arquivo();
-    sorteio_pontuacao();
-
+*P_pont_rodada = valores_pontos[v[7]];
 }
